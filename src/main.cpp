62e906da33e5e6fc616ae0fb093989018b4a40fb
Krzysztof Bobnis <kbobnis@gmail.com>
2022-01-30 16:07:08 +0100

    added font

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,18 @@ int main()
 	Board board(CellsCountHorizontal, CellsCountVertical, cellWidth, cellHeight, 1);
 	Clock clock;
 
+	//Setup score counter
+	sf::Text score;
+	sf::Font font;
+	font.loadFromFile("../Forwa_font.TTF");
+	score.setFont(font);
+	score.setCharacterSize(15);
+	score.setFillColor(sf::Color::Blue);
+	score.setPosition(10,25);
+	sf::Vector2<float> score_scale(1.5f,1.5f);
+	score.setScale(score_scale);
+	score.setString("Lines: 0");
+
 	while (window.isOpen())
 	{
 		sf::Event e;
@@ -51,6 +63,7 @@ int main()
 
 		window.clear();
 		board.draw(&window);
+		window.draw(score);
 		window.display();
 	}
 }
